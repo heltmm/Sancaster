3690107105e5317ee237ff49706417a336d02832
Keegan Ruebling and Mark Helt <mark@>
2017-08-08 10:34:56 -0700

    Add buttons that change color of backround and text, enlarge p tag font.

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,19 @@ $(document).ready(function() {
   $("#clickableUs").click(function() {
     $("#us").slideToggle();
   });
-});
-
-$(document).ready(function() {
   $("#clickableEu").click(function() {
     $("#eu").slideToggle();
   });
-});
-
-$(document).ready(function() {
   $("#clickableAu").click(function() {
     $("#au").slideToggle();
+  });
+  $("button#dark").click(function(){
+    $("body").removeClass();
+    $("body").addClass("lightDark");
+
+  });
+  $("button#light").click(function(){
+    $("body").removeClass();
+    $("body").addClass("darkLight");
   });
 });
